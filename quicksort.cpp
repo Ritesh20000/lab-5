5b90495ed589259a898e0eec8e85a653750936e5
Ritesh20000 <42208407+Ritesh20000@users.noreply.github.com>
2019-02-10 19:06:25 +0530

    Add files via upload

---
+++ quicksort.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+
+using namespace std;
+
+void quick_sort (int[], int, int);
+int partition (int[], int, int);
+
+int
+main ()
+{
+ 
+ int a[] = {1, 2, 4, 3};
+  quick_sort (a, 0,  3);
+  cout << "Array after sorting:";
+int i;
+  for (i = 0; i < 4; i++)
+    cout << a[i] << " ";
+
+  return 0;
+}
+
+void
+quick_sort (int a[], int l, int u)
+{
+  int j;
+  if (l < u)
+    {
+      j = partition (a, l, u);
+      quick_sort (a, l, j - 1);
+      quick_sort (a, j + 1, u);
+    }
+}
+
+int
+partition (int a[], int l, int u)
+{
+  int v, i, j, temp;
+  v = a[l];
+  i = l;
+  j = u + 1;
+
+  do
+    {
+      do
+	i++;
+
+      while (a[i] < v && i <= u);
+
+      do
+	j--;
+      while (v < a[j]);
+
+      if (i < j)
+	{
+	  temp = a[i];
+	  a[i] = a[j];
+	  a[j] = temp;
+	}
+    }
+  while (i < j);
+
+  a[l] = a[j];
+  a[j] = v;
+
+  return (j);
+}
